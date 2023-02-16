3c1ec3c50e7900d7eef2e586f6e76c765aa69ecb
Alotab <alotab@yahoo.com>
2023-02-16 01:45:31 +0000

    adding files including dockerfile

--- web/django_k8s/wsgi.py
+++ web/django_k8s/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import pathlib  #imported
+import dotenv    #imported
 
 from django.core.wsgi import get_wsgi_application
+
+CURRENT_DIR = pathlib.Path(__file__).resolve().parent   #work on this
+BASE_DIR = CURRENT_DIR.parent 
+ENV_FILE_PATH = BASE_DIR / ".env"
+
+dotenv.read_dotenv(str(ENV_FILE_PATH))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_k8s.settings')
 
